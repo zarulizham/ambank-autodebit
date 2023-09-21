5be3f771a6dc2a6cd367e2687d12f109a435dbcb
Zarul Zubir <zarul.izham@gmail.com>
2023-09-21 16:59:28 +0800

    Fix consent enquiry path

--- src/Messages/ConsentEnquiry.php
+++ src/Messages/ConsentEnquiry.php
@@ -6,25 +6,24 @@ use Illuminate\Support\Facades\Http;
 use ZarulIzham\AutoDebit\Traits\HasAuthorizedHeader;
 use ZarulIzham\AutoDebit\Traits\HasHttpResponse;
 use ZarulIzham\AutoDebit\Traits\HasSignature;
-use ZarulIzham\AutoDebit\Traits\HasSrcRefNo;
 
 class ConsentEnquiry
 {
-    use HasSignature;
-    use HasHttpResponse;
     use HasAuthorizedHeader;
+    use HasHttpResponse;
+    use HasSignature;
 
     public function enquiry(string $consentId)
     {
         $this->commonHeader();
 
-        $url = config('autodebit.url').'/api/EConsent/v1.0/ConsentReg/'.$this->sourceReferenceNumber;
+        $url = config('autodebit.url').'/api/EConsent/v1.0/ConsentEnquiry/'.$this->sourceReferenceNumber;
 
         $body = [
             'consentId' => $consentId,
         ];
 
-        $headers['Ambank-Signature'] = $this->signature('/api/EConsent/v1.0/ConsentReg/'.$this->sourceReferenceNumber, $this->ambankTimestamp, $body);
+        $headers['Ambank-Signature'] = $this->signature('/api/EConsent/v1.0/ConsentEnquiry/'.$this->sourceReferenceNumber, $this->ambankTimestamp, $body);
 
         $this->response = Http::withHeaders($headers)
             ->withOptions([
